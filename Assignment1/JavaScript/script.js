68516204afa97d70d8c997143a43abad46c6ff5e
Manju M <manju.m@coddletech.com>
2023-07-18 15:01:21 +0530

    commited

---
+++ Assignment1/JavaScript/script.js
@@ -0,0 +1,19 @@
+// Question 1
+var name = "Manju";
+var age = 22;
+var hasJob = true;
+var a;
+var b = null;
+var greeting = function() {
+    console.log("Hi");
+}
+let numbers = [1,2,3,4,5];
+var person = { firstName:"manju", lastNmae: " m"}
+
+console.log("variable: name, type:", typeof name)
+console.log("variable: age, type:", typeof age)
+console.log("variable: hasJob, type:", typeof hasJob)
+console.log("variable: a, type:", typeof a)
+console.log("variable: b, type:", typeof b)
+console.log("variable: greeting, type:", typeof greeting)
+console.log("variable: person, type:", typeof person)
